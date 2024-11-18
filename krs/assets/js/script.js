5276a13fc9b2a77e5250ed347071a645ea8b032a
Ilya <dulyuk20051712@gmail.com>
2024-11-18 16:33:04 +0300

    commit

--- krs/assets/js/script.js
+++ krs/assets/js/script.js
@@ -3,14 +3,35 @@ window.addEventListener('DOMContentLoaded', () => {
     // Поиск
 
     const search = document.querySelector('.search');
-    const searchOpenButton = document.querySelector('.search__open-button');
-    const searchCloseButton = document.querySelector('.serach__close-button');
+    const searchOpenButtons = document.querySelectorAll('.search__open-button');
+    const searchCloseButtons = document.querySelectorAll('.serach__close-button');
 
     function toggleSearch() {
         search.classList.toggle('active');
     }
 
-    searchOpenButton.addEventListener('click', toggleSearch);
-    searchCloseButton.addEventListener('click', toggleSearch);
+    searchOpenButtons.forEach(button => {
+        button.addEventListener('click', toggleSearch)
+    });
+    searchCloseButtons.forEach(button => {
+        button.addEventListener('click', toggleSearch)
+    });
+
+    // Меню
+
+    const menuDropdown = document.querySelector('.menu-dropdown');
+    const menuDropdownTrigger = document.querySelectorAll('.menu-dropdown-trigger');
+
+    function toggleMenuDropdown(button) {
+        menuDropdown.classList.toggle('active');
+        document.body.classList.toggle('no-scroll');
+        button.classList.toggle('active');
+    }
+
+    menuDropdownTrigger.forEach(button => {
+        button.addEventListener('click', () => {
+            toggleMenuDropdown(button);
+        });
+    })
 
 })
